776fa1d0dc0e4c67eb2d128275ac9cce9691a913
Bdon1066 <124404215+Bdon1066@users.noreply.github.com>
2025-01-28 19:56:51 +0000

    Completed Robot Mover
    Additionally: Created Player in scene, ready to start the RobotController

---
+++ Assets/_Project/Script/Player/RobotMover.cs
@@ -0,0 +1,134 @@
+using UnityEngine;
+
+
+/*
+<summary>
+ This class is for moving the robot mode
+ TODO: Create either mode base class or mode interface to allow creation of a "CarMover" thing
+</summary>
+*/
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]  
+public class RobotMover : MonoBehaviour
+{
+    [Header("Collider Settings")] 
+    [Range(0f,1f)][SerializeField] float stepHeightRatio = 0.1f;
+    [SerializeField] float colliderHeight = 2f;
+    [SerializeField] float colliderThickness = 1f;
+    [SerializeField] Vector3 colliderOffset = Vector3.zero;
+    
+    Rigidbody rb;
+    Transform tr;
+    CapsuleCollider col;
+    RaycastSensor sensor;
+    
+    bool isGrounded;
+    float baseSensorRange;
+    Vector3 currentGroundAdjustmentVelocity; //The velocity needed to adjust the player pos to correct ground pos over one frame
+    int currentLayer;
+
+    [Header("Sensor Settings:")] 
+    [SerializeField] bool debugMode;
+
+    bool usingExtendedSensorRange = true; //For use pn uneven terrain
+
+    void Awake()
+    {
+        Setup();
+        RecalculateColliderDimensions();
+    }
+    void Setup()
+    {
+        rb = GetComponent<Rigidbody>();
+        tr = GetComponent<Transform>();
+        col = GetComponent<CapsuleCollider>();
+        
+        rb.useGravity = false;
+        rb.freezeRotation = true;
+    }
+    void OnValidate()
+    {
+        if (gameObject.activeInHierarchy)
+        {
+            RecalculateColliderDimensions();
+        }
+    }
+    public void CheckForGround()
+    {
+        if (currentLayer != gameObject.layer)
+        {
+            RecalculateSensorLayerMask();
+        }
+        
+        currentGroundAdjustmentVelocity = Vector3.zero;
+        sensor.castLength =  usingExtendedSensorRange 
+            ? baseSensorRange + colliderHeight * tr.localScale.x * stepHeightRatio 
+            : baseSensorRange;
+        sensor.Cast();
+
+        isGrounded = sensor.HasDetectedHit();
+        if (!isGrounded) return;
+
+        float distance = sensor.GetDistance();
+        float upperLimit = colliderHeight * tr.localScale.x * (1f - stepHeightRatio) * 0.5f; //half the collider height above step area, top boundary of ideal pos
+        float middle = upperLimit + colliderHeight * tr.localScale.x * stepHeightRatio; // where the feet should be relative to ground
+        float distanceToGo = middle - distance;
+        
+        currentGroundAdjustmentVelocity = tr.up *(distanceToGo / Time.fixedDeltaTime);
+    }
+    public bool IsGrounded() => isGrounded;
+    public Vector3 GetGroundNormal() => sensor.GetNormal();
+    
+    public void SetVelocity(Vector3 velocity) => rb.velocity = velocity + currentGroundAdjustmentVelocity;
+    public void SetExtendSensorRange(bool isExtended) => usingExtendedSensorRange = isExtended;
+    void RecalculateColliderDimensions()
+    {
+        if (col == null) //i.e. in editor mode, need to run setup as Awake won't have been called
+        {
+            Setup();
+        }
+        
+        col.height = colliderHeight * (1f - stepHeightRatio);
+        col.radius = colliderThickness / 2f;
+        Vector3 stepHeightOffset = new Vector3(0f, stepHeightRatio * col.height / 2f, 0f); 
+        col.center = colliderOffset * colliderHeight + stepHeightOffset;
+
+        if (col.height / 2f < col.radius) //ensure radius is not too large and creating invalid capsule shape
+        {
+            col.radius = col.height / 2f;
+        }
+        
+        RecalibrateSensor();
+    }
+   
+
+    void RecalibrateSensor()
+    {
+        sensor ??= new RaycastSensor(tr); //ensures we have a sensor
+        
+        sensor.SetCastOrigin(col.bounds.center);
+        sensor.SetCastDirection(RaycastSensor.CastDirection.Down);
+        RecalculateSensorLayerMask();
+
+        const float safetyDistanceFactor = 0.001f; //Mysterious and fun factor to prevent clipping
+
+        float length = colliderHeight * (1f - stepHeightRatio) * 0.5f + colliderThickness / 2f * stepHeightRatio; //length of our ray, includes adjust collider height and step height ratio
+        baseSensorRange = length * (1f- safetyDistanceFactor) * tr.localScale.x;
+        sensor.castLength = length * tr.localScale.x;
+    }
+    void RecalculateSensorLayerMask()
+    {
+        int objectLayer = gameObject.layer;
+        int layerMask = Physics.AllLayers;
+
+        for (int i = 0; i < 32; i++) //iterate through all layers
+        {
+            if (Physics.GetIgnoreLayerCollision(objectLayer, i)) //if object's layer ought to ignore layer i
+            {
+                layerMask &= ~(1 << i); //remove layer i from the mask via magical bitshifting
+            }
+        }
+
+        int ignoreRaycastLayer = LayerMask.GetMask("Ignore Raycast"); // and do the same with ignore raycast layer
+        layerMask &= ~(1 << ignoreRaycastLayer);
+    }
+}
--- Assets/_Project/Script/RaycastSensor.cs
+++ Assets/_Project/Script/RaycastSensor.cs
@@ -1,15 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
+
+/*
+ * This is a generic raycast script that
+ * can be used for a bunch of stuff
+ */
 public class RaycastSensor
 {
     public float castLength = 1.0f;
-    public LayerMask layermask = 255;
+    public LayerMask layerMask = 255; //using numbers for layers is easier
 
     Vector3 origin = Vector3.zero;
     Transform tr;
 
     public enum CastDirection {Forward, Right, Up, Backward, Left, Down }
     CastDirection castDirection;
+    
+    RaycastHit hitInfo;
+
+    // constructor to initialize class with player Transform
+    public RaycastSensor(Transform playerTransform)
+    {
+        tr = playerTransform;
+    }
+    public void Cast()
+    {
+        //We need world origin and direction as opposed to local
+        Vector3 worldOrigin = tr.TransformPoint(origin);
+        Vector3 worldDirection = GetCastDirection();
+        
+        Physics.Raycast(worldOrigin, worldDirection, out hitInfo, castLength, layerMask,QueryTriggerInteraction.Ignore);
+    }
+    
+    public bool HasDetectedHit() => hitInfo.collider != null;
+    public float GetDistance() => hitInfo.distance;
+    public Vector3 GetNormal() => hitInfo.normal;
+    public Vector3 GetPosition() => hitInfo.point;
+    public Collider GetCollider() => hitInfo.collider;
+    public Transform GetTransform() => hitInfo.transform;
+    
+    public void SetCastDirection(CastDirection direction) => castDirection = direction;
+    public void SetCastOrigin(Vector3 pos) => origin = tr.InverseTransformPoint(pos);
+    
+    //Gets the actual vector direction from the corresponding enum
+    Vector3 GetCastDirection()
+    {
+        return castDirection switch
+        {
+            CastDirection.Forward => tr.forward,
+            CastDirection.Right => tr.right,
+            CastDirection.Up => tr.up,
+            CastDirection.Backward => -tr.forward,
+            CastDirection.Left => -tr.right,
+            CastDirection.Down => -tr.up,
+            _ => Vector3.one
+        };
+    }
 }
+
