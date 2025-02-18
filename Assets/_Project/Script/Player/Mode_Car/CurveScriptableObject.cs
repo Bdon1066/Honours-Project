9437a76383125d8e6986a802b0bdff6585115b5b
Bdon1066 <brandonmooney0@gmail.com>
2025-02-18 18:56:24 +0000

    Improved car handling by lerping steering and varying grip

---
+++ Assets/_Project/Script/Player/Mode_Car/CurveScriptableObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "CurveScriptableObject")]
+public class CurveScriptableObject : ScriptableObject
+{
+    public AnimationCurve curve;
+    
+    public float Evaluate(float time)
+    {
+        return curve.Evaluate(time);
+    }
+}
+
+
